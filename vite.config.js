415618b53dd522a3d01ff5b18a521c8ef330bc2f
Tianran-W <529708894@qq.com>
2025-09-25 14:04:36 +0800

    build: :sparkles: 添加构建流程

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Lincept.github.io/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
